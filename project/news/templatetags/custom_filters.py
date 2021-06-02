2ecbe704f19a10e1c1fa5b8aa515ccc7d8debb35
marbaterry <marbaterry@gmail.com>
2021-06-02 21:07:36 +0500

    add custom_filters, add static

--- project/news/templatetags/custom_filters.py
+++ project/news/templatetags/custom_filters.py
@@ -4,7 +4,20 @@ register = template.Library()
 
 
 @register.filter(name='censor')
-def censor(value, arg):
-    if isinstance(value, str) and isinstance(arg, int):
-        return str
+def censor(value):
+    obscene_values = [
+        'word1', 'word2', 'word3', 'title_number_4'
+    ]
+    if isinstance(value, str):
+        for i in value.split():
+            if i in obscene_values:
+                value = value.replace(i, '***')
+    return value
 
+
+@register.filter(name='multiply')
+def multiply(value, arg):
+    if isinstance(value, str) and isinstance(arg, int):
+        return str(value) * arg
+    else:
+        raise ValueError(f'Нельзя умножить {type(value)} на {type(arg)}')
--- project/project/settings.py
+++ project/project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news'
+    'news',
+    'news.templatetags'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
